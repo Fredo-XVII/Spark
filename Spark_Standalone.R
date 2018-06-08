02c392b90816bb97414796c47f3b511388f83f42
Alfredo.G.Marquez <Fredo-XVII@users.noreply.github.com>
2018-06-08 10:03:35 -0500

    Create Spark_Standalone.R

---
+++ Spark_Standalone.R
@@ -0,0 +1,14 @@
+## this connection to standalone on your machine for Spark
+
+# Testing Spark ----- not needed to run on bigRED
+
+library(SparkR, lib.loc = c(file.path(Sys.getenv("SPARK_HOME"), "R", "lib")))
+library(rJava) # didn't need this to get below to work.
+
+# note: did not run Start up file to get this to work...start_up changes the Java directory, don't forget!!!!
+sparkR.session(enableHiveSupport = TRUE ,
+               master = "local[*]", 
+               sparkHome = Sys.getenv("SPARK_HOME") , # this was the missing link!!
+               sparkConfig = list(spark.driver.memory = "2g", 
+                                  spark.sql.warehouse.dir="C:\\Apps\\winutils\\winutils-master\\hadoop-2.7.1")
+)
