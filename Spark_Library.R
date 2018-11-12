a93a566b09148ee439b4b24664fbee83a46e76a4
Alfredo.G.Marquez <Fredo-XVII@users.noreply.github.com>
2018-11-12 14:13:21 -0600

    Create Spark_Library.R

---
+++ Spark_Library.R
@@ -0,0 +1,16 @@
+Sys.setenv("SPARK_HOME" = "/usr/share/spark-shell") # "/usr/local/")
+
+#finds SparkR in bigRED
+library(SparkR, lib.loc = c(file.path(Sys.getenv("SPARK_HOME"), "R", "lib")))
+
+# Create a SparkR session...connecting to Spark on bigRED from the edge node
+sparkR.session(appName = "XXXXX" ,
+               master = "yarn" ,
+               mode = "cluster" ,
+               sparkConfig = list(spark.driver.memory = "5g", spark.executor.memory = "25g" ,
+                                  spark.driver.cores = "250", spark.rpc.message.maxSize="1024")
+               )
+
+# finds Hive Metadata warehouse on bigRED
+sparkR.session(enableHiveSupport = TRUE)
+}
