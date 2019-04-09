2d48b7671726ee517a37069ae33176eea8a66ce9
Alfredo.G.Marquez <Fredo-XVII@users.noreply.github.com>
2019-04-09 14:43:30 -0500

    Update Spark_Library.R

--- Spark_Library.R
+++ Spark_Library.R
@@ -8,7 +8,10 @@ sparkR.session(appName = "XXXXX" ,
                master = "yarn" ,
                mode = "cluster" ,
                sparkConfig = list(spark.driver.memory = "5g", spark.executor.memory = "25g" ,
-                                  spark.driver.cores = "250", spark.rpc.message.maxSize="1024")
+                                  spark.driver.cores = "250", spark.rpc.message.maxSize="1024",
+                                  spark.sql.crossJoin.enabled=TRUE, spark.sql.shuffle.partitions = "2000",
+                                  spark.serializer = "org.apache.spark.serializer.KryoSerializer",
+                                  spark.rdd.compress = "true")
                )
 
 # finds Hive Metadata warehouse on bigRED
