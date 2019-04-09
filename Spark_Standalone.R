1f0ad12bd063c8744689d0ee3aa22846a7b7bc67
Alfredo.G.Marquez <Fredo-XVII@users.noreply.github.com>
2019-04-09 14:41:09 -0500

    Update Spark_Standalone.R

--- Spark_Standalone.R
+++ Spark_Standalone.R
@@ -10,6 +10,9 @@ sparkR.session(enableHiveSupport = TRUE ,
                master = "local[*]", # master = "yarn", mode = "cluster",
                sparkHome = Sys.getenv("SPARK_HOME") , # this was the missing link!!  
                 sparkConfig = list(spark.driver.memory = "10g", spark.executor.memory = "22g", spark.driver.cores = "250",
-                                   spark.rpc.message.maxSize = "1024", spark.sql.crossJoin.enabled=TRUE
+                                   spark.rpc.message.maxSize = "1024", spark.sql.crossJoin.enabled=TRUE,
+                                   spark.sql.shuffle.partitions = "2000",
+                                   spark.serializer = "org.apache.spark.serializer.KryoSerializer",
+                                   spark.rdd.compress = "true"
                                   spark.sql.warehouse.dir="C:\\Apps\\winutils\\winutils-master\\hadoop-2.7.1")
 )
